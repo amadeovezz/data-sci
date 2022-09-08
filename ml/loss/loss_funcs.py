6b98749af5c3e2e6713a15f90b16f9cf2e88e81b
Amadeo <6462800+amadeovezz@users.noreply.github.com>
2022-09-08 17:50:58 -0400

    rename loss functions

--- ml/loss/loss_funcs.py
+++ ml/loss/loss_funcs.py
@@ -1,16 +1,16 @@
 
 
-def one_zero_loss(x: int) -> int:
+def one_zero(x: int) -> int:
     if x >= 0:
         return 0
     return 1
 
 
-def hinge_loss(x: float) -> float:
+def hinge(x: float) -> float:
     if x >= 1:
         return 0
     return 1 - x
 
 
-def least_squares(x: float) -> float:
+def squared_error(x: float) -> float:
     return x**2
